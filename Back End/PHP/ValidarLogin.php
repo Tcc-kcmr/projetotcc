6a50cb76a2b895aee9ec4db14fd6f213109c87a3
Tcc-kcmr <77458768+Tcc-kcmr@users.noreply.github.com>
2021-02-10 22:07:21 -0300

    Create ValidarLogin.php

---
+++ Back End/PHP/ValidarLogin.php
@@ -0,0 +1,47 @@
+
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-type: application/json");
+
+$dados = [];
+
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+
+    $Json = json_decode(file_get_contents('php://input'), true);
+
+    if($Json){
+
+    }
+
+    $Login = $Json['login'];
+    $Senha = $Json['senha'];
+
+    include 'Conexao.php'; 
+
+    $sql = "SELECT COUNT(usuario) as usuario FROM login where usuario = '$Login' and senha = '$Senha'";
+
+    $resultado = mysqli_query($conexao, $sql);
+
+    while ($dado = mysqli_fetch_assoc($resultado)){
+
+        $dados[] = $dado;
+
+    }
+
+    if($dados[0]["usuario"]==1){
+        
+        $mensagem=1; //1=> login válido! Validar no Js
+
+    }else{
+        $mensagem=0; //0=> login inválido!
+    }
+
+    echo json_encode($mensagem);
+
+}else{
+    $retorno["Mensagem"] = "Utilize o método GET";
+    echo json_encode($retorno);
+}
+
+?>
