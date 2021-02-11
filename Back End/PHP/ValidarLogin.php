3ab13bc74b0426b7fd52c50f361ec869c3d4eb81
Tcc-kcmr <77458768+Tcc-kcmr@users.noreply.github.com>
2021-02-11 17:45:58 -0300

    Update ValidarLogin.php

--- Back End/PHP/ValidarLogin.php
+++ Back End/PHP/ValidarLogin.php
@@ -10,34 +10,37 @@ if($_SERVER['REQUEST_METHOD'] == "GET"){
 
     $Json = json_decode(file_get_contents('php://input'), true);
 
-    if($Json){
+    if($Json != null){
 
-    }
+        $Login = $Json['login'];
+        $Senha = $Json['senha'];
 
-    $Login = $Json['login'];
-    $Senha = $Json['senha'];
+        include 'Conexao.php'; 
 
-    include 'Conexao.php'; 
+        $sql = "SELECT COUNT(usuario) as usuario FROM login where usuario = '$Login' and senha = '$Senha'";
 
-    $sql = "SELECT COUNT(usuario) as usuario FROM login where usuario = '$Login' and senha = '$Senha'";
+        $resultado = mysqli_query($conexao, $sql);
 
-    $resultado = mysqli_query($conexao, $sql);
+        while ($dado = mysqli_fetch_assoc($resultado)){
 
-    while ($dado = mysqli_fetch_assoc($resultado)){
+            $dados[] = $dado;
 
-        $dados[] = $dado;
+        }
 
-    }
+        if($dados[0]["usuario"]==1){
+            
+            $mensagem=1; //1=> login válido! Validar no Js
 
-    if($dados[0]["usuario"]==1){
-        
-        $mensagem=1; //1=> login válido! Validar no Js
+        }else{
+            $mensagem=0; //0=> login inválido!
+        }
+
+        echo json_encode($mensagem);
 
     }else{
-        $mensagem=0; //0=> login inválido!
+        $retorno["Mensagem"] = "ERRO na transferência de arquivos!!!";
+        echo json_encode($retorno);
     }
-
-    echo json_encode($mensagem);
 
 }else{
     $retorno["Mensagem"] = "Utilize o método GET";
