dd656cdbdef5604ead719d2360a905679136fb0c
Tcc-kcmr <77458768+Tcc-kcmr@users.noreply.github.com>
2021-03-06 19:11:11 -0300

    Create ConsultarConsultas.php

---
+++ Back End/PHP/ConsultarConsultas.php
@@ -0,0 +1,40 @@
+<?php
+
+//Essa Página, consulta as consultas já marcados
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Content-type: application/json");
+
+$dados = [];
+
+if($_SERVER['REQUEST_METHOD'] == "GET"){
+
+    $Dados = json_decode(file_get_contents('php://input'), true);
+
+    if($Dados != null){
+
+        include 'Conexao.php'; 
+        $data = $Dados["data"];
+
+        $sql = "SELECT p.nome AS nome, p.telefone AS telefone, p.email AS email, s.data AS data, s.hor_ini AS hor_ini, es.nome AS especialidade  FROM pessoa AS p INNER JOIN paciente AS pa ON (p.id = pa.id_pessoa) INNER JOIN servico AS s ON (pa.id_pessoa = s.id_paciente) INNER JOIN especialidade AS es ON (es.id = s.id_especialidade) WHERE s.nome = 'consulta' AND s.data = ''";
+       
+        $resultado = mysqli_query($conexao, $sql);
+
+        while ($dado = mysqli_fetch_assoc($resultado)){
+
+            $dados[] = $dado;
+
+        }
+
+        echo json_encode($dados);
+    }else{
+        $retorno["Mensagem"] = "Erro ao enviar os dados";
+        echo json_encode($retorno);
+    }
+}else{
+    $retorno["Mensagem"] = "Utilize o método POST";
+    echo json_encode($retorno);
+}
+
+?>
